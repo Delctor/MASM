182a4f9905b178c20c20b76081a789729c77dde5
Delctor <89162148+Delctor@users.noreply.github.com>
2023-09-21 22:26:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 
-extern "C" int func();
+extern "C" void random_generator(uint64_t seed, size_t size, double* arr);
 
 int main()
 {
+	uint64_t seed, size;
+	std::cout << "Enter a seed: ";
 
-	std::cout << func() << std::endl;
+	std::cin >> seed;
+
+	std::cout << "Enter the number of random numbers: ";
+
+	std::cin >> size;
+
+	double* arr = new double[size];
+
+	random_generator(seed, size, arr);
+
+	for (size_t i = 0; i < size; i++)
+	{
+		std::cout << arr[i] << std::endl;
+	}
+
+	delete[] arr;
 
 	return 0;
 }
